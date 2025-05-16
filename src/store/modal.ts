d3925ff22b8e3369ef4906292e137b5c9b118c75
shining013 <nevertheless113@naver.com>
2025-05-16 10:57:56 +0900

    feat: modal slice

--- src/store/modal.ts
+++ src/store/modal.ts
@@ -1,2 +1,26 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ModalType, ShowModalProps } from "../interface";
 
+const initialState: ModalType = {
+    showModal: false,
+    date: "",
+    id: null,
+};
+
+export const modalSlice = createSlice({
+    name: "calendar",
+    initialState,
+    reducers: {
+        setModalState: (state, action: PayloadAction<boolean>) => {
+            state.showModal = action.payload;
+        },
+        setOpenModal: (state, action: PayloadAction<ShowModalProps>) => {
+            state.date = action.payload.date;
+            state.id = action.payload.id;
+        },
+    },
+});
+
+export const { setModalState, setOpenModal } = modalSlice.actions;
+
+export default modalSlice.reducer;
